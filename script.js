27086bdd5a9fb5c980646807882dba50a5f0a80d
Heri23F <heri.github.com@gmail.com>
2026-09-16 16:46:56 +0700

    add gameControl modules

--- script.js
+++ script.js
@@ -1,63 +1,113 @@
 const GameBoard = function () {
-  const columns = 3;
   const rows = 3;
-  const board = [];
+  const columns = 3;
+  let board = [];
 
-  const newBoard = () => {
+  const makeBoard = () => {
     for (let i = 0; i < rows; i++) {
       board[i] = [];
       for (let j = 0; j < columns; j++) {
-        board[i].push(cell());
+        board[i].push(cell(i, j));
       }
     }
   };
 
-  const cell = () => {
+  const cell = (indexRow, indexColumn) => {
     let value = 0;
     const getValue = () => value;
     const changeValue = (newValue) => (value = newValue);
-
     return { getValue, changeValue };
-  };
-  const displayBoard = () => {
-    if (board.length === 0) return;
-    return board.map((row) => row.map((column) => column.getValue()));
-  };
-
-  const addPlayerToken = (token, row, column) => {
-    if (board[row][column].getValue() !== 0) return;
-    board[row][column].changeValue(token);
   };
 
   const getBoard = () => board;
+  const boardState = () =>
+    board.map((row) => row.map((cell) => cell.getValue()));
 
-  return { getBoard, newBoard, displayBoard, addPlayerToken };
+  function addMark(mark, row, column) {
+    //guardrail for inputing cell that already taken
+    if (board[row][column].getValue() !== 0) return "invalid";
+
+    return board[row][column].changeValue(mark);
+  }
+  const ownCell = (mark) => {
+    let ownList = [];
+    board.forEach((row, rowIndex) => {
+      row.forEach((column, columIndex) => {
+        if (column.getValue() === mark) {
+          return ownList.push([rowIndex, columIndex]);
+        }
+      });
+    });
+
+    return ownList;
+  };
+
+  return { getBoard, boardState, addMark, makeBoard, ownCell };
 };
 
-const GameControl = (playerOne = "Player One", playerTwo = "Player Two") => {
-  const board = GameBoard();
-  board.newBoard();
-
+const gameControl = function (
+  playerOne = "Player One",
+  playerTwo = "Player Two",
+) {
   const player = [
     {
       name: playerOne,
-      token: 1,
+      mark: 1,
     },
     {
       name: playerTwo,
-      token: 2,
+      mark: 2,
     },
   ];
+  let board = GameBoard();
+  board.makeBoard();
+  let turn = 0;
 
-  const turn = (() => {
-    let currentTurn = 0;
+  const switchTurn = () => (turn === 0 ? (turn = 1) : (turn = 0));
+  const getTurn = () => player[turn].name;
+  const playRound = function (row, column) {
+    if (board.addMark(player[turn].mark, row, column) === "invalid") {
+      return console.log(displayTurn.errorTurn());
+    }
 
-    const switchTurn = () =>
-      currentTurn === 0 ? (currentTurn = 1) : (currentTurn = 0);
-    const getTurn = () => currentTurn;
+    board.addMark(player[turn].mark, row, column);
+    displayTurn.takeTurn();
+    switchTurn();
+    displayTurn.newTurn();
+  };
 
-    return { getTurn, switchTurn };
+  // used for console based not needed for ui
+  const displayTurn = (() => {
+    const takeTurn = () => {
+      console.log(getTurn() + " take turn");
+      console.log(board.boardState());
+    };
+    const newTurn = () => console.log(`Now ${getTurn()} Turn`);
+    const errorTurn = () => `Cell already taken still ${getTurn()} turn`;
+
+    return { takeTurn, newTurn, errorTurn };
   })();
+
+  const getPlayerCell = () => {
+    let playerCell = {
+      playerOne: board.ownCell(player[0].mark),
+      playerTwo: board.ownCell(player[1].mark),
+    };
+
+    return playerCell;
+  };
+
+  return { playRound, getPlayerCell };
 };
 
-let game = GameControl("Heri", "Heru");
+let game = gameControl();
+
+function simulateTurn() {
+  game.playRound(0, 0);
+  game.playRound(0, 2);
+  game.playRound(1, 0);
+  game.playRound(1, 1);
+  game.playRound(2, 0);
+}
+
+simulateTurn();
